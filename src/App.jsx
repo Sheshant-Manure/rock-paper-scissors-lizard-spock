5a2a19c8c3aa2e254347a12aac582bd7968d1435
Sheshant Manure <sheshant@Sheshants-MacBook-Air.local>
2025-12-02 15:11:35 +0530

    basic symbols of the game

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import "./App.css";
+import paperImg from "./assets/paper.png";
+import scissors1Img from "./assets/scissors.png";
+import rockImg from "./assets/rock.png";
+import lizardImg from "./assets/lizard.png";
+import spockImg from "./assets/spock.png";
 
 const App = () => {
-  return <div></div>;
+  const images = [
+    { src: paperImg, alt: "paper" },
+    { src: scissors1Img, alt: "scissors 1" },
+    { src: rockImg, alt: "rock" },
+    { src: lizardImg, alt: "lizard" },
+    { src: spockImg, alt: "spock" },
+  ];
+
+  return (
+    <div className="image-container">
+      {images.map((image, index) => (
+        <img
+          key={index}
+          src={image.src}
+          alt={image.alt}
+          className="asset-image"
+        />
+      ))}
+    </div>
+  );
 };
 
 export default App;
